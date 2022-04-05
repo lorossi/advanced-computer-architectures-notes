197cd9f382f0009dbee2ff2e68139af6c3f53425
lorossi <mail@lorenzoros.si>
2022-04-05 08:37:53 +0200

    environment settings

--- envsetup.py
+++ envsetup.py
@@ -1,12 +1,15 @@
 from re import sub, M
+from pathlib import Path
 
+
+TEMP_FOLDER = "tikztemp"
 ARROW_STYLE = "{Triangle[scale=0.8]}"
 
 
 def clean_styles() -> None:
     """cleans the tikzstyles file as it was created by tikzit"""
     # load styles files
-    with open(f"tikzit.tikzstyles", "r") as f:
+    with open("tikzit.tikzstyles", "r") as f:
         data = f.read()
 
     # remove tikzit attributes
@@ -23,5 +26,10 @@ def clean_styles() -> None:
         f.write(data)
 
 
+def make_folder() -> None:
+    Path(TEMP_FOLDER).mkdir(parents=True, exist_ok=True)
+
+
 if __name__ == "__main__":
     clean_styles()
+    make_folder()
